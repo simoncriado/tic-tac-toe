0fe0a70b56fa43fe7f5ef547aa8a01d0267bb8f1
Simon Criado <50947741+simoncriado@users.noreply.github.com>
2022-05-12 09:18:16 +0200

    Add files via upload

---
+++ my-app/build/precache-manifest.a30d37da11e084908387b267877fe5d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc357c3fc5da3d7ee65e075b9f8a35bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46913090a3c975349650",
+    "url": "/static/css/main.4afa2deb.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "/static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "46913090a3c975349650",
+    "url": "/static/js/main.14b7dd69.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
